d31040a499451c1ea3ee8ebe08394870bf133298
Aleksandr Kimball-Moreno <alekskimball@gmail.com>
2023-01-17 08:49:05 -0800

    more layout/styles updates

--- comic-book-app/components/Header.tsx
+++ comic-book-app/components/Header.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Image from "next/image";
 import styles from "../styles/Header.module.css";
 import logo from "../public/logo.svg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
 
 
 export function Header() {
@@ -14,14 +17,15 @@ export function Header() {
 				/>
 			</div>
 			<ul className={styles.header_list}>
-				<li>
+				<li className={styles.header_list_item}>
 					<a>Home</a>
 				</li>
-				<li>
-					<a>Shop</a></li>
-				<li>
-					<i></i>
-					<a>My Favorites</a>
+				<li className={styles.header_list_item}>
+					<a>Shop</a>
+				</li>
+				<li className={styles.header_list_item}>
+					<FontAwesomeIcon icon={faBoltLightning} />
+					<a>My Favorites (3)</a>
 				</li>
 			</ul>
 		</nav>
--- comic-book-app/components/Hero.tsx
+++ comic-book-app/components/Hero.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
 import hero_desktop from "../public/hero-photo@2x.png"
+import halftone_desktop from "../public/halftone@2x.png"
 import styles from "../styles/Hero.module.css";
 
 export function Hero() {
 	return (
-		<div>
+		<div className={styles.hero_container}>
 			<Image
-				className={styles.hero_image}
+				className={styles.hero_image_desktop}
 				src={hero_desktop}
 				alt="comic hero desktop"
+			/>
+			<Image
+				className={styles.halftone_image_desktop}
+				src={halftone_desktop}
+				alt="comic halftone desktop"
 			/>
 			<div className={styles.hero_banner}>
 				<h1 className={styles.hero_title}>Comic Closet</h1>
